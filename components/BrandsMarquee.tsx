cb5b693545b52836b1d1c454d8173ac6e75d23a6
ahmedmagdy1987 <ahmedkassim17777@gmail.com>
2026-07-04 17:04:46 +0300

    fix: seamless full-width partner marquee (ltr + -50% loop), lock brand logo lockup to ltr so it reads R.Pay

--- components/BrandsMarquee.tsx
+++ components/BrandsMarquee.tsx
@@ -1,18 +1,8 @@
 "use client";
 import { LOGOS } from "@/lib/assets/logos";
 
-function Track() {
-  return (
-    <div className="mtrack" aria-hidden="true">
-      {LOGOS.map((l, i) => (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img key={i} className="brandlogo" src={l.uri} alt={l.alt} loading="lazy" />
-      ))}
-    </div>
-  );
-}
-
 export default function BrandsMarquee() {
+  const row = [...LOGOS, ...LOGOS];
   return (
     <section className="brands" aria-label="partners">
       <div className="lbl">
@@ -20,8 +10,12 @@ export default function BrandsMarquee() {
         <span className="en-t">Our partners &amp; clients</span>
       </div>
       <div className="marquee">
-        <Track />
-        <Track />
+        <div className="mtrack">
+          {row.map((l, i) => (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img key={i} className="brandlogo" src={l.uri} alt={l.alt} loading="lazy" />
+          ))}
+        </div>
       </div>
     </section>
   );
